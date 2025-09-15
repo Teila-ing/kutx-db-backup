4c4ae77e89536f9a95dd9b1ed7f8de3766c7ce5e
Teila-ing <Teila-ing@users.noreply.github.com>
2025-09-15 01:09:47 +0000

    Supabase backup

---
+++ backups/2025-09-15_01-07-53/roles.sql
@@ -0,0 +1,17 @@
+
+\restrict pG6NHEXdO9rpeTaMFNstSKNHKpFkeJ7yOBWo9ErGqoGSht7PaF4QrBdcGtNlKFZ
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+ALTER ROLE "anon" SET "statement_timeout" TO '3s';
+
+ALTER ROLE "authenticated" SET "statement_timeout" TO '8s';
+
+ALTER ROLE "authenticator" SET "statement_timeout" TO '8s';
+
+\unrestrict pG6NHEXdO9rpeTaMFNstSKNHKpFkeJ7yOBWo9ErGqoGSht7PaF4QrBdcGtNlKFZ
+
+RESET ALL;
